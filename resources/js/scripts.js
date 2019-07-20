80fef7504c3cb926f26f39fbd88278f4be90668d
Eirik Hop <eirik.hop@gmail.com>
2019-07-20 15:16:24 +0200

    initial commit, complete website

---
+++ resources/js/scripts.js
@@ -0,0 +1,69 @@
+/* global $ */
+/* eslint-env jquery */
+
+$('document').ready(function() {
+    
+    /* For the sticky nagivation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+    offset: '60px'
+    });
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+
+    /* mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-ios-menu')) {
+            icon.addClass('ion-ios-close');
+            icon.removeClass('ion-ios-menu');
+        } else {
+            icon.addClass('ion-ios-menu');
+            icon.removeClass('ion-ios-close');
+        }    
+    });
+    
+});
+
